9cc45e5f45b26a0d23559fe700e1efba4d2c68ac
Wilson Silva <wilson.dsigns@gmail.com>
2023-06-25 10:56:34 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Pokédex
+
+The Pokédex (ポケモン 図鑑 ずかん , Pokemon Zukan, Illustrated Pokémon Encyclopedia) is an electronic
+device created and designed to catalog and provide information regarding the various species of
+Pokémon featured in the Pokémon video game, anime and manga series.
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
